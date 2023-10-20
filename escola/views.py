67efda0880a50fd65f183ee3da9ae67cd55bb566
joao <jaopiovezam@gmail.vom>
2023-10-20 16:33:43 -0300

    ft - pesquisa por id

--- escola/views.py
+++ escola/views.py
@@ -1,11 +1,15 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from escola.models import Aluno, Curso, Matricula
-from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer 
+from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer, ListaAlunoMatriculadosSerializer 
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class AlunosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os alunos e alunas"""
     queryset = Aluno.objects.all()
     serializer_class = AlunoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class CursosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os cursos"""
@@ -16,3 +20,17 @@ class MatriculasViewSet(viewsets.ModelViewSet):
     """Exibindo todos as Matriculas"""
     queryset = Matricula.objects.all()
     serializer_class = MatriculaSerializer
+    
+class ListaMatriculasAluno(generics.ListAPIView):
+    """Listando as matriculas do Aluno"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(aluno_id = self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasAlunoSerializer
+    
+class ListaAlunosMatriculados(generics.ListAPIView):
+    """Listando alunos matriculados em um curso"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id = self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaAlunoMatriculadosSerializer
