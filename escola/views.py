24a2acad81d698664556dfe09cc5f9e9f935e154
joao <jaopiovezam@gmail.vom>
2023-10-20 10:34:13 -0300

    ft - amtricula

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,18 @@
-from django.http import JsonResponse
+from rest_framework import viewsets
+from escola.models import Aluno, Curso, Matricula
+from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer 
 
-def alunos(request):
-    if request.method == 'GET':
-        aluno = {'id': 1, 'nome':'Jão'}
-        return JsonResponse(aluno)
+class AlunosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os alunos e alunas"""
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+class CursosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os cursos"""
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+    
+class MatriculasViewSet(viewsets.ModelViewSet):
+    """Exibindo todos as Matriculas"""
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
