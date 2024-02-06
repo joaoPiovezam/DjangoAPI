9199db0f49c6c633e168e6f61477590cdd828be7
joao <jaopiovezam@gmail.vom>
2024-02-06 14:58:20 -0300

    questionario

---
+++ diagnostico/models.py
@@ -0,0 +1,76 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length = 255)
+    
+    def __str__(self):
+        return self.nome
+
+class Questionario(models.Model):
+    class Meta:
+        verbose_name = _("Questionário")
+        verbose_name_plural = _("Questionários")
+        ordering = ['id']
+        
+    titulo = models.CharField(max_length = 255, default = _(
+        "Novo Questionário"), verbose_name = _("Título do Questionário"
+    ))
+    categoria = models.ForeignKey(
+        Categoria, default = 1, on_delete = models.DO_NOTHING
+    )
+    data_criacao = models.DateTimeField(auto_now_add = True, verbose_name = _("Data de Criação"))
+    
+    def __str__(self):
+        return self.titulo
+
+class Atualizada(models.Model):
+    data_atualizada = models.DateTimeField(
+        verbose_name = _("Última Atualização"), auto_now = True
+    )
+    
+    class Meta:
+        abstract = True
+
+class Pergunta(Atualizada):
+    
+    class Meta:
+        verbose_name = _("Pergunta")
+        verbose_name_plural = _("Perguntas")
+        ordering = ['id']
+        
+    Escala = (
+        (1, _('Muito Pouco')),
+        (2, _('Pouco')),
+        (3, _('Razoavel')),
+        (4, _('Ok')),
+        (5, _('Muito')),
+    )
+    
+    TIPO = (
+        (0, _('Multipla Escolha'))
+    )
+    
+    questionario = models.ForeignKey(
+        Questionario, related_name = 'pergunta', on_delete = models.DO_NOTHING
+    )
+    tecnica = models.IntegerField(choices = TIPO, default = 0, verbose_name = _("Tipo de Pergunta"))
+    titulo = models.CharField(verbose_name = _("Titulo"), max_length=255)
+    opcao = models.IntegerField(choices = Escala, default = 3, verbose_name = _("Opção"))
+    data_criacao = models.DateTimeField(auto_now_add = True, verbose_name = _("Data de Criação"))
+    esta_ativo = models.BooleanField(default = True, verbose_name = _("Status de Ativação"))
+    
+    def __str__(self):
+        return self.titulo
+
+class Resposta(Atualizada):
+    
+    class Meta:
+        verbose_name = _("Resposta")
+        verbose_name_plural = _("Respostas")
+        ordering = ['id']
+    
+    pergunta = models.ForeignKey(
+        Pergunta, related_name = 'resposta', on_delete = models.DO_NOTHING
+    )
+    resposta_texto = models.CharField(max_length = 255, verbose_name = _("Resposta Texto"))
--- setup/urls.py
+++ setup/urls.py
@@ -1,16 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from loja.views import PecasViewSet, ClientesViewSet, OrcamentoViewSet, PedidoViewSet, PedidoOrcamentoViewSet
 
 router = routers.DefaultRouter()
-router.register('pecas', PecasViewSet, basename='Pecas')
-router.register('clientes', ClientesViewSet, basename='Clientes')
-router.register('orcamentos', OrcamentoViewSet, basename='Orcamentos')
-router.register('pedidos', PedidoViewSet, basename='Pedidos')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include(router.urls)),
-    path('orcamento/<int:pk>/pedidos/',PedidoOrcamentoViewSet.as_view()),
 ]
